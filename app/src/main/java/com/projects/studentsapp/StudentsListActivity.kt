c1e579dc2bbac2d51467150b3fdca5ce563ceb41
shiryjona <shiryj@cs.colman.ac.il>
2026-01-29 20:33:31 +0200

    fixing imports and UI

--- app/src/main/java/com/projects/studentsapp/StudentsListActivity.kt
+++ app/src/main/java/com/projects/studentsapp/StudentsListActivity.kt
@@ -1,8 +1,10 @@
 package com.projects.studentsapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.projects.studentsapp.databinding.ActivityStudentsListBinding
 import com.projects.studentsapp.model.Model
 
 class StudentsListActivity : AppCompatActivity() {
